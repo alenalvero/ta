4f2a1ecc78912233d1b8ce7067911c165b2417e8
Bayu Fanani <fanani707@gmail.com>
2019-06-19 11:02:01 +0700

    improve ui, ux, and code of trayek

--- resources/views/trayek/create.blade.php
+++ resources/views/trayek/create.blade.php
@@ -3,115 +3,79 @@
 @section('title', 'Warok Tour')
 
 @section('content_header')
-    <h1>
-        Data Tables
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Tables</a></li>
-        <li class="active">Data tables</li>
-      </ol>
-    
-
+  <h1>
+    Data Tables
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="#">Tables</a></li>
+    <li class="active">Data tables</li>
+  </ol>
 @stop
 
 @section('content')
-   
-    <section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header">
-              <table>
+  <section class="content">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="box">
+          <div class="box-header">
             <h2>
               <button class="btn btn-block btn-danger btn-flat"  type="button">Edit Data Trayek</button></a>
             </h2>
-            </table>
-            </div>
-            <!-- /.box-header -->
-        
+          </div>
           <form class="form-horizontal" action="{{url('operator/trayek')}}" method="post">
             {{csrf_field()}}
-            
             <div class="box-body">
               <div class="form-group">
                 <label class="col-sm-2 control-label" for="asal" >Asal</label>
-                  <div class="col-sm-10">
-                    <input  class="form-control" placeholder="Masukkan Asal" name="asal" type="text">
-                  </div>
+                <div class="col-sm-10">
+                  <input  class="form-control" placeholder="Masukkan Asal" name="asal" type="text">
+                </div>
               </div>
-              
-             <div class="form-group">
-              
+              <div class="form-group">
                 <label class="col-sm-2 control-label" for="id_kota" >Tujuan</label>
-                  <div class="col-sm-10">
-                    <select name="id_kota" id="kota" class="form-control">
+                <div class="col-sm-10">
+                  <select name="id_kota" id="kota" class="form-control">
                     @foreach ($kotas as $item)
                     <option value="{{ $item->id }}">{{$item->nama_kota}}</option>  
                     @endforeach
-                    </select>
-                  </div>
+                  </select>
+                </div>
               </div>
               <div class="form-group">
-              
                 <label class="col-sm-2 control-label" for="id_tempat_wisata" >Tempat Wisata</label>
-                  <div class="col-sm-10">
-                    <select name="id_tempat_wisata" id="tempat_wisata" class="form-control">
-                    
-                    </select>
-                  </div>
+                <div class="col-sm-10">
+                  <select name="id_tempat_wisata" id="tempat_wisata" class="form-control">
+                  </select>
+                </div>
               </div>
-
-
-
-
-
-
-
-
-                           
-
-                </tfoot>
-              
             </div>
-          
-            <!-- /.box-body -->
-              <div class="box-footer">
-                <button class="btn btn-info btn-flat pull-right" type="submit">Submit </button>
-              </div>
-            </form>
-          </div>
-          <!-- /.box -->
-
-          
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
+            <div class="box-footer">
+              <button class="btn btn-info btn-flat pull-right" type="submit">Submit </button>
+            </div>
+          </form>
         </div>
-        <!-- /.col -->
       </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
-  </div>
+    </div>
+  </section>
 @stop
 
 
 @section('adminlte_js')
-<script>
-$(document).ready(function() {
-  $('#kota').change(function() {
-    //console.log($('#kota').val())
-    $.ajax({
-      url: '/get_wisata/'+$('#kota').val(),
-      success: function(data) {
-        $('#tempat_wisata').children().remove();
-        for(var i = 0; i < data.length; i++) {
-          $('#tempat_wisata').append("<option value='"+data[i].id+"'>"+data[i].nama+"</option>");
+  <script>
+  $(document).ready(function() {
+    $('#kota').change(function() {
+      //console.log($('#kota').val())
+      $.ajax({
+        url: '/get_wisata/'+$('#kota').val(),
+        success: function(data) {
+          $('#tempat_wisata').children().remove();
+          for(var i = 0; i < data.length; i++) {
+            $('#tempat_wisata').append("<option value='"+data[i].id+"'>"+data[i].nama+"</option>");
+          }
         }
-      }
+      });
     });
-  });
-})
-</script>
+  })
+  </script>
 @stop
--- resources/views/trayek/index.blade.php
+++ resources/views/trayek/index.blade.php
@@ -4,78 +4,55 @@
 
 @section('content_header')
     <h1>
-        Data Tables
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Tables</a></li>
-        <li class="active">Data tables</li>
-      </ol>
-    
-
+      Data Trayek
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Trayek</li>
+    </ol>
 @stop
 
 @section('content')
-   
-    <section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header">
-              <table>
-            <td>
-              <a href='{{url('operator/trayek/create') }}'><button class="btn bg-maroon btn-flat margin"  type="button">Create</button></a>
-            </td>
+  <section class="content">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="box">
+          <div class="box-header">
+            <table>
+              <td>
+                <a href='{{url('operator/trayek/create') }}'><button class="btn bg-maroon btn-flat margin"  type="button">Create</button></a>
+              </td>
             </table>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-
-              <table id="example2" class="table table-bordered table-hover">
-                <thead>
-                <tr>
-                  <th>Asal</th>
-                  <th>Tujuan</th>
-                  <th>Tempat Wisata</th>
-                  
-                  <th>Aksi</th>  
-                </tr>
-                </thead>
-                <tbody>
-@foreach($trayek as $item)
+          </div>
+          <div class="box-body">
+            <table id="example2" class="table table-bordered table-hover">
+              <thead>
+              <tr>
+                <th>Asal</th>
+                <th>Tujuan</th>
+                <th>Tempat Wisata</th>
+                <th>Aksi</th>  
+              </tr>
+              </thead>
+              <tbody>
+                @foreach($trayek as $item)
                 <tr>
                   <td>{{$item->asal}}</td>
-                  <td>{{$item->kota->nama_kota}}
-                  </td>
+                  <td>{{$item->kota->nama_kota}}</td>
                   <td>{{$item->tempat_wisata->nama}}</td>
-                  
-                 
-                
-            <td> 
-              <div class="btn-group">
- <a href='{{url('operator/trayek/destroy/'.$item->id) }}'> <button class="btn btn-danger btn-flat btn-xs" type="button"><i class="fa fa-align-left"></i></button>
- <a href='{{url('operator/trayek/'.$item->id.'/edit') }}'><button class="btn btn-info btn-flat btn-xs" type="button"><i class="fa fa-align-right"></i></button></a>             
-            </div>
-            </td>
-                
+                  <td> 
+                    <div class="btn-group">
+                      <a href='{{url('operator/trayek/'.$item->id.'/edit') }}'><button class="btn btn-info btn-flat btn-xs" type="button"><i class="fa fa-edit"></i></button></a>
+                      <a href='{{url('operator/trayek/destroy/'.$item->id) }}' onclick="return confirm('Apa anda yakin ingin menghapus ini?')"> <button class="btn btn-danger btn-flat btn-xs" type="button"><i class="fa fa-trash"></i></button>
+                    </div>
+                  </td>
                 </tr>
- @endforeach 
-                </tfoot>
-              </table>
-            </div>
-            <!-- /.box-body -->
+                @endforeach 
+              </tbody>
+            </table>
           </div>
-          <!-- /.box -->
-
-          
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
         </div>
-        <!-- /.col -->
       </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
-  </div>
+    </div>
+  </section>
 @stop
