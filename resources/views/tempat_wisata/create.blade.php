89f3ac9ad09c66b072c59227933f036363bf010e
Bayu Fanani <fanani707@gmail.com>
2019-06-19 11:12:54 +0700

    improve ui, ux, and code of tempat_wisata

--- resources/views/tempat_wisata/create.blade.php
+++ resources/views/tempat_wisata/create.blade.php
@@ -3,89 +3,58 @@
 @section('title', 'Warok Tour')
 
 @section('content_header')
-    <h1>
-        Data Tables
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Tables</a></li>
-        <li class="active">Data tables</li>
-      </ol>
-    
-
+  <h1>
+    Data Tables
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="#">Tables</a></li>
+    <li class="active">Data tables</li>
+  </ol>
 @stop
 
 @section('content')
-   
-    <section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header">
-              <table>
+  <section class="content">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="box">
+          <div class="box-header">
             <h2>
               <button class="btn btn-block btn-danger btn-flat"  type="button">Create Data Tempat Wisata</button></a>
             </h2>
-            </table>
-            </div>
-            <!-- /.box-header -->
-        
+          </div>
           <form class="form-horizontal" action="{{url('operator/tempat_wisata')}}" method="post">
             {{csrf_field()}}
             <div class="box-body">
               <div class="form-group">
                 <label class="col-sm-2 control-label" for="nama" >Nama Tempat Wisata</label>
-                  <div class="col-sm-10">
-                    <input  class="form-control" placeholder="Masukkan Nama Tempat Wisata" name="nama" type="text">
-                  </div>
+                <div class="col-sm-10">
+                  <input  class="form-control" placeholder="Masukkan Nama Tempat Wisata" name="nama" type="text">
+                </div>
               </div>
-              
               <div class="form-group">
-              
                 <label class="col-sm-2 control-label" for="id_kota" >Kota</label>
-                  <div class="col-sm-10">
-                    <select name="id_kota" class="form-control">
+                <div class="col-sm-10">
+                  <select name="id_kota" class="form-control">
                     @foreach ($kotas as $item)
                     <option value="{{ $item->id }}">{{$item->nama_kota}}</option>  
                     @endforeach
-                    </select>
-                  </div>
+                  </select>
+                </div>
               </div>
-              
               <div class="form-group">
                 <label class="col-sm-2 control-label" for="harga" >Harga</label>
-                  <div class="col-sm-10">
-                    <input name="harga" class="form-control" placeholder="Masukkan Harga" type="text">
-                  </div>
+                <div class="col-sm-10">
+                  <input name="harga" class="form-control" placeholder="Masukkan Harga" type="text">
+                </div>
               </div>
-              
-                    
-              
-                       
-
-                            
- 
-                </tfoot>
-              
             </div>
-          
-            <!-- /.box-body -->
-              <div class="box-footer">
-                <button class="btn btn-info btn-flat pull-right" type="submit">Submit </button>
-              </div>
-            </form>
-          </div>
-          <!-- /.box -->
-
-          
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
+            <div class="box-footer">
+              <button class="btn btn-info btn-flat pull-right" type="submit">Submit </button>
+            </div>
+          </form>
         </div>
-        <!-- /.col -->
       </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
-  </div>
+    </div>
+  </section>
 @stop
--- resources/views/tempat_wisata/index.blade.php
+++ resources/views/tempat_wisata/index.blade.php
@@ -3,88 +3,60 @@
 @section('title', 'Warok Tour')
 
 @section('content_header')
-    <h1>
-        Data Tables
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Tables</a></li>
-        <li class="active">Data tables</li>
-      </ol>
-    
-
+  <h1>
+    Data Tables
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="#">Tables</a></li>
+    <li class="active">Data tables</li>
+  </ol>
 @stop
 
 @section('content')
-   
-    <section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header">
-              <table>
-            <td>
-              <a href='{{url('operator/tempat_wisata/create') }}'><button class="btn bg-maroon btn-flat margin"  type="button">Create</button></a>
-            </td>
+  <section class="content">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="box">
+          <div class="box-header">
+            <table>
+              <td>
+                <a href='{{url('operator/tempat_wisata/create') }}'><button class="btn bg-maroon btn-flat margin"  type="button">Create</button></a>
+              </td>
             </table>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-
-              <table id="example2" class="table table-bordered table-hover">
-                <thead>
-                <tr>
-                  <th>Nama</th>
-                  <th>Kota</th>
-                  <th>Harga</th>
-                  
-                  <th>Aksi</th>  
-                </tr>
-                </thead>
-                <tbody>
-@foreach($tempat_wisata as $tempat)
+          </div>
+          <div class="box-body">
+            <table id="example2" class="table table-bordered table-hover">
+              <thead>
+              <tr>
+                <th>Nama</th>
+                <th>Kota</th>
+                <th>Harga</th>
+                <th>Aksi</th>  
+              </tr>
+              </thead>
+              <tbody>
+                @foreach($tempat_wisata as $tempat)
                 <tr>
                   <td>{{$tempat->nama}}</td>
-                  <td>{{$tempat->kota->nama_kota}}
-                  </td>
+                  <td>{{$tempat->kota->nama_kota}}</td>
                   <td>{{$tempat->harga}}</td>
-                  
-                 
-                
-            <td> 
-              <div class="btn-group">
-    <a href='{{url('operator/tempat_wisata/destroy/'.$tempat->id) }}'>    <button class="btn btn-danger btn-flat btn-xs" type="button"><i class="fa fa-align-left"></i></button></a>
-    <a href='{{url('operator/tempat_wisata/'.$tempat->id.'/edit') }}'><button class="btn btn-info btn-flat btn-xs" type="button"><i class="fa fa-align-right"></i></button></a>             
-            </div>
-  
-            </td>
-                
+                  <td> 
+                    <div class="btn-group">
+                      <a href='{{url('operator/tempat_wisata/'.$tempat->id.'/edit') }}'><button class="btn btn-info btn-flat btn-xs" type="button"><i class="fa fa-edit"></i></button></a>
+                      <a href='{{url('operator/tempat_wisata/destroy/'.$tempat->id) }}' onclick="return confirm('Anda yakin ingin menghapus ini?')"><button class="btn btn-danger btn-flat btn-xs" type="button"><i class="fa fa-trash"></i></button></a>
+                    </div>
+                  </td>
                 </tr>
-
- @endforeach 
-
-              
-
-                </tfoot>
-              </table>
-            </div>
-            <!-- /.box-body -->
+                @endforeach
+              </tbody>
+            </table>
+          </div>
           <div class="box-footer">
           {!! $tempat_wisata->render() !!}
-              </div>
-          
           </div>
-          <!-- /.box -->
-
-          
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
         </div>
-        <!-- /.col -->
       </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
-  </div>
+    </div>
+  </section>
 @stop
