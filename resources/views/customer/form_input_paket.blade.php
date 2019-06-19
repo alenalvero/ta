3f945a54a809b8dd1311b55ec2f0f234d4491174
Bayu Fanani <fanani707@gmail.com>
2019-06-19 19:16:07 +0700

    update date picker for disable functio

--- resources/views/customer/form_input_paket.blade.php
+++ resources/views/customer/form_input_paket.blade.php
@@ -18,28 +18,26 @@ border-width: 5px;
 @stop
 
 @section('content')
-		<aside id="colorlib-hero">
-			<div class="flexslider">
-				<ul class="slides">
-			   	<li style="background-image: url('{{asset('images/img_bg_5.jpg')}}');">
-			   		<div class="overlay"></div>
-			   		<div class="container-fluid">
-			   			<div class="row">
-				   			<div class="col-md-6 col-sm-12 col-md-offset-3 slider-text">
-				   				<div class="slider-text-inner slider-text-inner2 text-center">
-				   					
-				   					<h1>Form Reservasi</h1>
-				   				</div>
-				   			</div>
-				   		</div>
-			   		</div>
-			   	</li>
-			  	</ul>
-		  	</div>
-		</aside>
-
-			<div id="colorlib-blog">
-			<div class="container">
+	<aside id="colorlib-hero">
+		<div class="flexslider">
+			<ul class="slides">
+				<li style="background-image: url('{{asset('images/img_bg_5.jpg')}}');">
+					<div class="overlay"></div>
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-md-6 col-sm-12 col-md-offset-3 slider-text">
+								<div class="slider-text-inner slider-text-inner2 text-center">
+									<h1>Form Reservasi</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+				</li>
+				</ul>
+			</div>
+	</aside>
+	<div id="colorlib-blog">
+		<div class="container">
 			<form action="{{url('customer_PaketTour')}}" method="post">
 				<div class="row">
 					<div class="col-md-7 col-md-push-5">
@@ -57,83 +55,81 @@ border-width: 5px;
 								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, qui quod ipsum harum id, minima nesciunt! Saepe soluta, vitae quas expedita voluptatem voluptates placeat numquam provident quis, atque nisi iure?</p>
 							</div>
 						</article>
-						
-						
 					</div>
-
 					<div class="col-md-4 col-md-pull-7">
 						<div class="aside animate-box">
 							<h3><b>Form Reservasi</b><hr></h3>
-						<form method="post" action="{{url('customer_PaketTour')}}" class="colorlib-form">
-			              	{{csrf_field()}}
-			              	<div class="row">
-			                <div class="col-md-12">
-			                  
-			                  
-			                    <div class="form-field">
-			                     
-			                      <input type="hidden" name="id_user" id="date" class="form-control" value="{{Auth::user()->id}}">
-			                    </div>
-			                  
-			                </div>
-			                <div class="col-md-12">
-			                  <div class="form-group">
-			                    <label for="nama">Nama Lengkap</label>
-			                    <div class="form-field">
-			                     
-			                      <input type="text" name="nama_pelanggan_paket" class="form-control" >
-			                    </div>
-			                  </div>
-			                </div>
-			                <div class="col-md-12">
-			                  <div class="form-group">
-			                    <label for="paket">Paket</label>
-			                    <div class="form-field">
-			                     
-			                      <input type="hidden" name="id_paket" value="{{ $paket_tour->id}}"  class="form-control" >
-			                      <input type="text" value="{{ $paket_tour->nama_tour}}"  class="form-control" >
-			                    </div>
-			                  </div>
-			                </div>
-			                <div class="col-md-12">
-			                  <div class="form-group">
-			                    <label for="alamat">Alamat</label>
-			                    <div class="form-field">
-			                     
-			                      <input type="text" name="alamat" class="form-control" >
-			                    </div>
-			                  </div>
-			                </div>
-			                <div class="col-md-12">
-			                  <div class="form-group">
-			                    <label for="no_telp">Nomer Telfon</label>
-			                    <div class="form-field">
-			                     
-			                      <input type="text" name="no_telp" class="form-control" >
-			                    </div>
-			                  </div>
-			                </div>
-			                <div class="col-md-12">
-			                  <div class="form-group">
-			                    <label for="tgl">Tanggal</label>
-			                    <div class="form-field">
-			                      <i class="icon icon-calendar2"></i>
-			                      <input type="text" name="tgl" id="date" class="form-control date" placeholder="Tanggal">
-			                    </div>
-			                  </div>
-			                </div>
-			                
-			                <div class="col-md-12">
-			                  <input type="submit" name="submit" id="submit" value="Simpan" class="btn btn-primary btn-block">
-			                </div>
-			              </div>
-			            </form>
+							<form method="post" action="{{url('customer_PaketTour')}}" class="colorlib-form">
+								{{csrf_field()}}
+								<div class="row">
+									<div class="col-md-12">
+										<div class="form-field">
+											<input type="hidden" name="id_user" id="date" class="form-control" value="{{Auth::user()->id}}">
+										</div>
+									</div>
+									<div class="col-md-12">
+										<div class="form-group">
+											<label for="nama">Nama Lengkap</label>
+											<div class="form-field">
+												<input type="text" name="nama_pelanggan_paket" class="form-control" >
+											</div>
+										</div>
+									</div>
+									<div class="col-md-12">
+										<div class="form-group">
+											<label for="paket">Paket</label>
+											<div class="form-field">
+												<input type="hidden" name="id_paket" value="{{ $paket_tour->id}}"  class="form-control" >
+												<input type="text" value="{{ $paket_tour->nama_tour}}"  class="form-control" >
+											</div>
+										</div>
+									</div>
+									<div class="col-md-12">
+										<div class="form-group">
+											<label for="alamat">Alamat</label>
+											<div class="form-field">
+												<input type="text" name="alamat" class="form-control" >
+											</div>
+										</div>
+									</div>
+									<div class="col-md-12">
+										<div class="form-group">
+											<label for="no_telp">Nomer Telfon</label>
+											<div class="form-field">
+												<input type="text" name="no_telp" class="form-control" >
+											</div>
+										</div>
+									</div>
+									<div class="col-md-12">
+										<div class="form-group">
+											<label for="tgl">Tanggal</label>
+											<div class="form-field">
+												<i class="icon icon-calendar2"></i>
+												<input type="text" name="tgl" id="tgl" class="form-control" placeholder="Tanggal">
+											</div>
+										</div>
+									</div>
+									<div class="col-md-12">
+										<input type="submit" name="submit" id="submit" value="Simpan" class="btn btn-primary btn-block">
+									</div>
+								</div>
+							</form>
 						</div>
-						
 					</div>
 				</div>
 			</form>
-			</div>
 		</div>
-		</div>
+	</div>
 @stop
+
+@section('script')
+	<script>
+		// var datesForDisable = ["7/4/2019", "7/9/2019", "7/15/2019"];
+		$('#tgl').datepicker({
+				format: 'm/d/yyyy',
+				autoclose: true,
+				startDate: new Date(),
+				// datesDisabled: ['6/20/2019'],
+		});
+	</script>
+@endsection
--- resources/views/customer/profile.blade.php
+++ resources/views/customer/profile.blade.php
@@ -1,6 +1,5 @@
 @extends('customer.master')
 
-@section('content')
 @section('css')
 <style type="text/css">/*hr {
 display: block;
@@ -33,54 +32,59 @@ table td, th {
     }
     </style>
 @stop
-	
-		
-		<div id="colorlib-about">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-3">
-						<div class="about animate-box">
-							<b><font size="5">ACCOUNT DETAIL</font></b>
-							
-						</div>
-						<div class="about animate-box">
-							<p><b>{{Auth::user()->name}}</b></p>
-							<p>		Warok Tour merupakan biro perjalanan wisata yang memudahkan
-							perjalanan wisata anda, tinggal duduk manis anda akan kami antar sesuai tujuan wisata anda.</p>
-						</div>
 
+@section('content')
+	<div id="colorlib-about">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="about animate-box">
+						<b><font size="5">ACCOUNT DETAIL</font></b>
 					</div>
-					<div class="col-md-1">
-						<div class="about animate-box">
-							<hr class="vertical">
-						</div>					
+					<div class="about animate-box">
+						<p>
+							Nama:<br>
+							<b>{{Auth::user()->name}}</b>
+						</p>
+						<p>
+							Email:<br>
+							<b>{{Auth::user()->email}}</b>
+						</p>
+						<p>
+							<a href="/profile/edit"><i class="fa fa-edit"></i> Edit profil</a>
+						</p>
+						<hr>
+						<p>		Warok Tour merupakan biro perjalanan wisata yang memudahkan
+						perjalanan wisata anda, tinggal duduk manis anda akan kami antar sesuai tujuan wisata anda.</p>
 					</div>
-					<div class="col-md-8">
+				</div>
+				<div class="col-md-1">
+					<div class="about animate-box">
+						<hr class="vertical">
+					</div>					
+				</div>
+				<div class="col-md-8">
 					<div class="about animate-box">
 						<b><font size="5">ORDER HISTORY</font></b>
 					</div>
 					<div class="about animate-box">
 						<p>*Click your ORDER-ID to see detail status</p>	
-					
 							<table border="1">
-								<p><tr bgcolor='#EE6868'>
+								<tr bgcolor='#EE6868'>
 									<th width="150" height="30" style="background-color: #eee;">&nbsp;&nbsp;ORDER</th>
 									<th width="200" style="background-color: #eee;">&nbsp;&nbsp;DATE</th>
 									<th width="200" style="background-color: #eee;">&nbsp;&nbsp;STATUS</th>
 									<th width="200" style="background-color: #eee;">&nbsp;&nbsp;TOTAL</th>
 								</tr>
-				@foreach ($profile as $item)
+								@foreach ($profile as $item)
 								<tr>
 									<td height="30"></td>
 									<td></td>
 									<td></td>
 									<td></td>
 								</tr>
-					@endforeach
-							
-							</p>
+								@endforeach
 							</table>
-							
 					</div>
 					<div class="about animate-box">
 						<p>*Click your ORDER-ID to see detail status</p>	
@@ -96,44 +100,38 @@ table td, th {
 									<td height="30"><a href='{{url('profile/detail_order/'.$item->id.'/show') }}'>{{$item->id}}</a></td>
 									<td>{{date('d/m/Y',strtotime($item->created_at))}}</td>
 									<td>{!!
-                  $item->konfirmasi_pembayaran != null ? $item->konfirmasi_pembayaran->status == 1 ? 'Sudah dibayar' : 'Menunggu verfikasi' : "<a href='".url('profile/konfirmasi_pembayaran/'.$item->id.'/konfirmasi')."'>"."Belum dibayar"."</a>"
-                  !!}</td>
+									$item->konfirmasi_pembayaran != null ? $item->konfirmasi_pembayaran->status == 1 ? 'Sudah dibayar' : 'Menunggu verfikasi' : "<a href='".url('profile/konfirmasi_pembayaran/'.$item->id.'/konfirmasi')."'>"."Belum dibayar"."</a>"
+									!!}</td>
 									<td>dsdsadsa</td>
 								</tr>
-					@endforeach
-							
+								@endforeach
 							</table>
-					{!! $profile1->render() !!}
+							{!! $profile1->render() !!}
 					</div>
-					
-					</div>
-
 				</div>
 			</div>
 		</div>
-
-		
-
-		<div id="colorlib-subscribe" style="background-image: url(images/img_bg_2.jpg);">
-			<div class="overlay"></div>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 col-md-offset-3 text-center colorlib-heading animate-box">
-						<span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span>
-						<h2>Sign Up for a Newsletter</h2>
-						<p></p>
-						<form class="form-inline qbstp-header-subscribe">
-							<div class="row">
-								<div class="col-md-12 col-md-offset-0">
-									<div class="form-group">
-										<input type="text" class="form-control" id="email" placeholder="Enter your email">
-										<button type="submit" class="btn btn-primary">Subscribe</button>
-									</div>
+	</div>
+	<div id="colorlib-subscribe" style="background-image: url(images/img_bg_2.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center colorlib-heading animate-box">
+					<span><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i><i class="icon-star-full"></i></span>
+					<h2>Sign Up for a Newsletter</h2>
+					<p></p>
+					<form class="form-inline qbstp-header-subscribe">
+						<div class="row">
+							<div class="col-md-12 col-md-offset-0">
+								<div class="form-group">
+									<input type="text" class="form-control" id="email" placeholder="Enter your email">
+									<button type="submit" class="btn btn-primary">Subscribe</button>
 								</div>
 							</div>
-						</form>
-					</div>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
+	</div>
 @stop		
