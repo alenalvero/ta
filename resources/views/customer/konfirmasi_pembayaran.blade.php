36de961f8bdb26ae46b5270e1c85a257b45df5ae
Bayu Fanani <fanani707@gmail.com>
2019-07-05 00:34:06 +0700

    update tampilan pemesanan paket

--- resources/views/customer/konfirmasi_pembayaran.blade.php
+++ resources/views/customer/konfirmasi_pembayaran.blade.php
@@ -21,21 +21,20 @@ border-width: 5px;
 		<aside id="colorlib-hero">
 			<div class="flexslider">
 				<ul class="slides">
-			   	<li style="background-image: url('{{asset('images/img_bg_5.jpg')}}');">
-			   		<div class="overlay"></div>
-			   		<div class="container-fluid">
-			   			<div class="row">
-				   			<div class="col-md-6 col-sm-12 col-md-offset-3 slider-text">
-				   				<div class="slider-text-inner slider-text-inner2 text-center">
-				   					
-				   					<h1>Form Reservasi</h1>
-				   				</div>
-				   			</div>
-				   		</div>
-			   		</div>
-			   	</li>
-			  	</ul>
-		  	</div>
+					<li style="background-image: url('{{asset('images/img_bg_5.jpg')}}');">
+						<div class="overlay"></div>
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-md-6 col-sm-12 col-md-offset-3 slider-text">
+									<div class="slider-text-inner slider-text-inner2 text-center">
+										<h1>Form Reservasi</h1>
+									</div>
+								</div>
+							</div>
+						</div>
+					</li>
+					</ul>
+				</div>
 		</aside>
 
 			<div id="colorlib-blog">
@@ -44,56 +43,59 @@ border-width: 5px;
 				<div class="row">
 					<div class="col-md-7 col-md-push-5">
 						<article class="animate-box">
-							<div class="blog-img" ></div>
-							<div class="desc">
-								<div class="meta">
-									<p>
-										<span>August 24, 2017 </span>
-										<span>admin </span>
-										<span><a href="#">2 Comments</a></span>
-									</p>
-								</div>
-								<h2><a href="#">A Definitive Guide to the Best Dining</a></h2>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, qui quod ipsum harum id, minima nesciunt! Saepe soluta, vitae quas expedita voluptatem voluptates placeat numquam provident quis, atque nisi iure?</p>
-							</div>
+							<h2>{{$pemesanan_paket_tour->paket_tour->nama_tour}}</h2>
+							<img src="{{url('images/'.$pemesanan_paket_tour->paket_tour->foto)}}"/>
+							<p>
+								<b>Deskripsi</b><br/>
+								{!!$pemesanan_paket_tour->paket_tour->keterangan!!}
+							</p>
+							<p>
+								<b>Nama Pelanggan</b><br/>
+								{{$pemesanan_paket_tour->nama_pelanggan_paket}}
+							</p>
+
+							<p>
+								<b>No. Telfon</b><br/>
+								{{$pemesanan_paket_tour->no_telp}}
+							</p>
+							<p>
+								<b>Alamat</b><br/>
+								{{$pemesanan_paket_tour->alamat}}
+							</p>
+							<p>
+								<b>Harga </b><br/>
+								{{$pemesanan_paket_tour->paket_tour->harga}}
+							</p>
+							<p>
+								<b>Tanggal Berangkat</b><br/>
+								{{$pemesanan_paket_tour->tgl}}
+							</p>
 						</article>
-						
-						
 					</div>
 
 					<div class="col-md-4 col-md-pull-7">
 						<div class="aside animate-box">
-							<h3><b>Form Konfirmasi Pembayaran</b><hr></h3>
+							<h3><b>Form Konfirmasi Pembayaran Paket</b><hr></h3>
 						<form method="post" action="{{url('profile/konfirmasi_pembayaran')}}" class="colorlib-form">
-			              	{{csrf_field()}}
-			              	<div class="row">
-			                <div class="col-md-12">
-			                  
-			                  
-			                    <div class="form-field">
-			                     <label for="id_pemesanan">Id Pemesanan</label>
-			                      <input type="text" name="id_pemesanan_paket" value="{{$pemesanan_paket_tour->id}}" id="date" class="form-control">
-			                    </div>
-			                  
-			                </div>
-			                <div class="col-md-12">
-			                  <div class="form-group">
-			                    <label for="Foto">Foto</label>
-			                    <div class="form-field">
-			                     
-			                      <input type="file" name="foto" >
-			                    </div>
-			                  </div>
-			                </div>
-			                
-			                
-			                <div class="col-md-12">
-			                  <input type="submit" name="submit" id="submit" value="Simpan" class="btn btn-primary btn-block">
-			                </div>
-			              </div>
-			            </form>
+									{{csrf_field()}}
+									<div class="row">
+									<div class="col-md-12">
+										<input type="hidden" name="id_pemesanan_paket" value="{{$pemesanan_paket_tour->id}}" id="date" class="form-control">
+										</div>
+									<div class="col-md-12">
+										<div class="form-group">
+											<label for="Foto">Foto</label>
+											<div class="form-field">
+												<input type="file" name="foto" >
+											</div>
+										</div>
+									</div>
+									<div class="col-md-12">
+										<input type="submit" name="submit" id="submit" value="Simpan" class="btn btn-primary btn-block">
+									</div>
+								</div>
+							</form>
 						</div>
-						
 					</div>
 				</div>
 			</form>
